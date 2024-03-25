cfa917e4e71516dafa1093259dbfc8527480be75
dgsw10 <kmj1110@dgsw10ui-noteubug.local>
2024-03-25 20:32:58 +0900

    commit

---
+++ s.c/s.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+  long long int a;
+  scanf("%lld", &a);
+  printf("%lld",a+1);
+	return 0;
+}
+
+echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/kmj1110/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
